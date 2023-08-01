2f4880bbd13fbac82aca7c0d482d39b7864dd6d1
ikedh <mcalderon@digitalhouse.com>
2023-08-01 12:37:06 -0300

    desde cero

---
+++ clase4.js
@@ -0,0 +1,12 @@
+
+console.log(document);
+
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                          CONSIGNA MESA DE TRABAJO                          */
+/* -------------------------------------------------------------------------- */
+// 1 - Escribir un selector para cada elemento del sitio.
+// 2 - Plasmarlo en un diagrama de árbol como la consigna: https://docs.google.com/document/d/15nGvKkEcbrRgwqV50ISh0QYZ_TO67vmWQaLfNpUxqjs/preview
+
